3f8fd6b6483b520d9934d895a6ca5eeef5e52fde
Hasib787 <mr.hasib45@gmail.com>
2021-01-22 19:32:40 +0600

    some changes

--- assignment.js
+++ assignment.js
@@ -1,19 +1,37 @@
 //problem-1: Kilometer To Metter
 function kilometerToMeter(kilometer) {
-    var metter = 1000;
-    kilometer = kilometer * metter;
+    var meter = 1000;
+    meter = meter * kilometer;
     if (kilometer <= 0) {
         return ('Zero and Negative value are not allow');
     } else {
-        return kilometer;
+        return meter;
     }
 }
-
 //problem-2: Budget Calculator
 function budgetCalculator(watch, mobile, laptop) {
     var perWatchPrice = 50;
     var perMobilePrice = 100;
     var perLaptopPrice = 500;
-    var totalPrice = (perWatchPrice*watch)+(perMobilePrice*mobile)+(perLaptopPrice*laptop);
-    return totalPrice;  
+    var totalPrice = (perWatchPrice * watch) + (perMobilePrice * mobile) + (perLaptopPrice * laptop);
+    return totalPrice;
 }
+//problem-3: Hotel Cost
+function hotelCost(days) {
+    var firstTenDays = 100;
+    var secondTenDays = 80;
+    var twentyDaysMore = 50;
+    if (days <= 0) {
+        return ('Zero and Negative value are not allow');
+    }
+    if (days <= 10) {
+        return firstTenDays * days;
+    } else if (days > 10 && days <= 20) {
+        return ((firstTenDays * 10) + (secondTenDays * days));
+    } else if (days > 20) {
+        return twentyDaysMore * days;
+    }
+    var totalCost = firstTenDays + secondTenDays + twentyDaysMore;
+    return totalCost;
+}
+//console.log(hotelCost(12));
